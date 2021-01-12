e712545d645cac5e542de006fd952ddf0eb09dd7
Ayan Sengupta <ayansengupta@umass.edu>
2021-01-12 12:31:07 -0500

    login page

---
+++ public/js/login.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    let passfield = $('#security');
+    $("#pass-submit").on("click",function(){
+        if(passfield.val()=="Money2020"){
+            location.href="../RegistrationA.html"
+        }
+        else{
+            let text = "";
+            passfield.text(text);
+            alert("Wrong Password");
+        }
+    });
+    
+    
+});
