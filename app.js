0a4bf886fac4fe0c74ed0a9a56b526fc1ea6cd26
Judge Russell <judgerussell@umass.edu>
2021-02-03 08:33:34 -0500

    cleaned up code, added comments, fixed sync issues ??????

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const app = express()
 
+// arrays to store current off/on values: false = off, true = on
 var status_reg_a = new Array(12).fill(false);
 var status_reg_b = new Array(12).fill(false);
 var status_obs_a = new Array(12).fill(false);
@@ -16,6 +17,8 @@ server = app.listen(3000)
 //socket.io instantiation
 const io = require("socket.io")(server)
 
+// this function changes the correct array, based on which page it is coming from
+// value is the value to set the button to, msg is a string which should contain a page name: regA, regB, obsA, or obsB
 function log_change(button, value, msg)
 {
 
@@ -37,6 +40,7 @@ function log_change(button, value, msg)
   }
 }
 
+// returns the correct status array given a page name
 function status(msg)
 {
   switch (msg){
@@ -54,111 +58,100 @@ function status(msg)
       break;
   } 
 }
+
+// adds listeners to socketio channels button# for # = 1 through 12
 function button(socket) {
   //button1
   socket.on('button1', (params) => {
     log_change(1, params[0], params[1])
-    io.emit('update', status(params[1]))
+    io.emit('update', [params[1], status(params[1])])
   });
   //button2
   socket.on('button2', (params) => {
     
     log_change(2, params[0], params[1])
-    io.emit('update', status(params[1]))
+    io.emit('update', [params[1], status(params[1])])
   });
   //button3
   socket.on('button3', (params) => {
     
     log_change(3, params[0], params[1])
-    io.emit('update', status(params[1]))
+    io.emit('update', [params[1], status(params[1])])
   });
   //button4
   socket.on('button4', (params) => {
     
     log_change(4, params[0], params[1])
-    io.emit('update', status(params[1]))
+    io.emit('update', [params[1], status(params[1])])
   });
   //button5
   socket.on('button5', (params) => {
     
     log_change(5, params[0], params[1])
-    io.emit('update', status(params[1]))
+    io.emit('update', [params[1], status(params[1])])
   });
   //button6
   socket.on('button6', (params) => {
     
     log_change(6, params[0], params[1])
-    io.emit('update', status(params[1]))
+    io.emit('update', [params[1], status(params[1])])
   });
   //button7
   socket.on('button7', (params) => {
     
     log_change(7, params[0], params[1])
-    io.emit('update', status(params[1]))
+    io.emit('update', [params[1], status(params[1])])
   });
   //button8
   socket.on('button8', (params) => {
     
     log_change(8, params[0], params[1])
-    io.emit('update', status(params[1]))
+    io.emit('update', [params[1], status(params[1])])
   });
   //button9
   socket.on('button9', (params) => {
     
     log_change(9, params[0], params[1])
-    io.emit('update', status(params[1]))
+    io.emit('update', [params[1], status(params[1])])
   });
   //button10
   socket.on('button10', (params) => {
     
     log_change(10, params[0], params[1])
-    io.emit('update', status(params[1]))
+    io.emit('update', [params[1], status(params[1])])
   });
   //button11
   socket.on('button11', (params) => {
     
     log_change(11, params[0], params[1])
-    io.emit('update', status(params[1]))
+    io.emit('update', [params[1], status(params[1])])
   });
   //button12
   socket.on('button12', (params) => {
     
     log_change(12, params[0], params[1])
-    io.emit('update', status(params[1]))
+    io.emit('update', [params[1], status(params[1])])
   });
 }
 
+// on connection to a new socket
 io.on('connection', (socket => {
+
+  // add button listeners
   button(socket)
 
+  // when server receives a message on the init channel from the socket
+  // set button values to what is stored in the particular status array
   socket.on('init', (msg) => {
    
     if(msg == 'regA') {
-  
-      for(i = 0; i < status_reg_a.length; i++)
-      {
-        if(status_reg_a[i])
-          io.emit('button' + (i + 1), [!status_reg_a[i], msg]);
-      }
+      io.emit('update', [msg, status_reg_a])
     } else if(msg == 'regB') {
-      for(i = 0; i < status_reg_b.length; i++)
-      {
-        if(status_reg_b[i])
-          io.emit('button' + (i + 1), [!status_reg_b[i], msg]);
-      }
+      io.emit('update', [msg, status_reg_b])
     } else if(msg == 'obsA') {
-  
-      for(i = 0; i < status_obs_a.length; i++)
-      {
-        if(status_obs_a[i])
-          io.emit('button' + (i + 1), [!status_obs_a[i], msg]);
-      }
+      io.emit('update', [msg, status_obs_a])
     } else if(msg == 'obsB') {
-      for(i = 0; i < status_obs_b.length; i++)
-      {
-        if(status_obs_b[i])
-          io.emit('button' + (i + 1), [!status_obs_b[i], msg]);
-      }
+      io.emit('update', [msg, status_obs_b])
     }
   });
 }));
--- public/js/buttonsOB.js
+++ public/js/buttonsOB.js
@@ -24,219 +24,96 @@ let button_ten= document.getElementById('ten');
 let button_eleven = document.getElementById('eleven');
 let button_twelve = document.getElementById('twelve');
 
+// array of button elements on page and their count, their current state
 let buttons = [button_one, button_two, button_three, button_four, button_five, button_six, button_seven, button_eight, button_nine, button_ten, button_eleven, button_twelve]
-let counts = [count1, count2, count3, count4, count5, count6, count7, count8, count9, count10, count11, count12]
 
-
+// set button status to false -- keeps track locally of the state
 var button_status = [false, false, false, false, false, false, false, false, false, false, false, false];
-console.log(button_status[0])
+
+// set up socket
 var socket = io();
 
-
+// emit init message
 socket.emit('init', 'obsB')
 
-socket.on('update', (s) => {
-    button_status = s
-    console.log(button_status)
-    for(let i = 0; button_status.length; i++) {
+// string ot hold page string
+let room = "obsB"
 
-        console.log(button_status[i])
-        if(button_status[i] == true)
-        {
-            buttons[i].style.backgroundColor ="#881c1c"
-        } else {    
-            buttons[i].style.backgroundColor ="#8f8d8d";   
+// when it receives an update message, take the new status array, and update each button to reflect it
+socket.on('update', (params) => {
+    msg = params[0]
+    if(msg == room) {
+        button_status = params[1]
+        console.log(button_status);
+        for(let i = 0; i < 12; i++) {
+
+            console.log(button_status[i])
+            if(button_status[i] == true)
+            {
+                buttons[i].style.backgroundColor ="#881c1c"
+            } else {    
+                buttons[i].style.backgroundColor ="#8f8d8d"  
+            }
         }
     }
 });
 
-let room = "obsB"
-
-function button_press(n, x)
-{
-    if(x==1)
-    {
-        counts[n] = counts[n] + 1
-    }
-    if(counts[n] == 1)
-    {
-        buttons[n].style.backgroundColor ="#881c1c"
-    }
-    else if(counts[n] == 2)
-    {
-        buttons[n].style.backgroundColor ="#8f8d8d";
-        counts[n] =0;
-    }
-}
-
+// return an array with the new button status and the page string
 function emit_info(button)
 {
     return [!button_status[button - 1], room]
 }
 
+// on click listeners to emit to the server to change button status
 $(function () {
     var socket = io();
     //button 1
     $('#one').click(function(e){
-        console.log(emit_info(1))
         socket.emit('button1', emit_info(1))
-    });
-    socket.on('button1', function(params){
-      if(params[1] == room && params[0] == counts[0])
-      {
-        button_press(0, 1);
-        console.log(count1);
-      }
     });
     //button 2
     $('#two').click(function(e){
-    
         socket.emit('button2',emit_info(2))
-        console.log('emitted')
-        return false;
-    });
-    socket.on('button2', function(params){
-      if(params[1] == room && params[0] == counts[1])
-      {
-        button_press(1, 1);
-        console.log(count2);
-      }
     });
     //button 3
     $('#three').click(function(e){
-    
-            socket.emit('button3',emit_info(3))
-            console.log('emitted')
-            return false;
-        
-    });
-    socket.on('button3', function(params){
-      if(params[1] == room && params[0] == counts[2])
-      {
-        button_press(2, 1);
-        console.log(count3);
-      }
+        socket.emit('button3',emit_info(3))
     });
     //button 4
     $('#four').click(function(e){
-
         socket.emit('button4',emit_info(4))
-        console.log('emitted')
-        return false;
-    });
-    socket.on('button4', function(params){
-        if(params[1] == room && params[0] == counts[3])
-        {
-            button_press(3, 1);
-            console.log(count4);
-        }
     });
     //button 5
     $('#five').click(function(e){
-    
         socket.emit('button5',emit_info(5))
-        console.log('emitted')
-        return false;
-    });
-    socket.on('button5', function(params){
-        if(params[1] == room && params[0] == counts[4])
-        {
-            button_press(4, 1);
-            console.log(count5);
-        }
     });
     //button 6
     $('#six').click(function(e){
     
         socket.emit('button6', emit_info(6))
-        console.log('emitted')
-        return false;
-    });
-    socket.on('button6', function(params){
-        if(params[1] == room && params[0] == counts[5])
-        {
-            button_press(5, 1);
-            console.log(count6);
-        }
     });
     //button 7
     $('#seven').click(function(e){
-    
         socket.emit('button7',emit_info(7))
-        console.log('emitted')
-        return false;
-        
-    });
-    socket.on('button7', function(params){
-      if(params[1] == room && params[0] == counts[6])
-      {
-        button_press(6, 1);
-        console.log(count7);
-      }
     });
     //button 8
     $('#eight').click(function(e){
-    
         socket.emit('button8',emit_info(8))
-        console.log('emitted')
-        return false;
-    });
-    socket.on('button8', function(params){
-      if(params[1] == room && params[0] == counts[0])
-      {
-        button_press(7, 1);
-        console.log(count8);
-      }
     });
     //button 9
     $('#nine').click(function(e){
-    
         socket.emit('button9',emit_info(9))
-        return false;
-    });
-    socket.on('button9', function(params){
-      if(params[1] == room && params[0] == counts[7])
-      {
-          button_press(8, 1);
-      }
     });
     //button 10
     $('#ten').click(function(e){
-
         socket.emit('button10',emit_info(10))
-        return false;
-    });
-    socket.on('button10', function(params){
-        if(params[1] == room && params[0] == counts[8])
-        {
-            button_press(9, 1);
-        }
     });
     //button 11
     $('#eleven').click(function(e){
-    
         socket.emit('button11', emit_info(11))
-        return false;
-    });
-    socket.on('button11', function(params){
-        if(params[1] == room && params[0] == counts[9])
-        {
-            button_press(10, 1);
-        }
     });
     //button 12
     $('#twelve').click(function(e){
-    
         socket.emit('button12', emit_info(12))
-        return false;
     });
-    socket.on('button12', function(params){
-        if(params[1] == room && params[0] == counts[10])
-        {
-            button_press(11, 1);
-        }
-    });
-
-    
-
-  });
+});
